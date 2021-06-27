d81afa689d63e1a1110aa92b545968af9072848d
Inklingboi <inklingboiii1@gmail.com>
2021-06-27 12:11:38 +0200

    added canvas resizing

--- src/App/components/Three.js
+++ src/App/components/Three.js
@@ -2,11 +2,24 @@ import * as THREE from 'three';
 
 export default class Three {
 	constructor(canvas) {
+		this.canvas = canvas;
 		this.canvasWidth = canvas.clientWidth;
 		this.canvasHeight = canvas.clientHeight;
+		window.addEventListener('resize', this.resizeCanvas);
 		this.scene = new THREE.Scene();
 		this.camera = new THREE.PerspectiveCamera(70, this.canvasWidth / this.canvasHeight, 0.1, 75);
 		this.renderer = new THREE.WebGLRenderer({canvas});
 		this.renderer.setSize(this.canvasWidth, this.canvasHeight, false);
+	}
+
+	resizeCanvas = () => {
+		this.canvasWidth = this.canvas.clientWidth;
+		this.canvasHeight = this.canvas.clientHeight;
+		if(this.canvasWidth !== this.canvas.width || this.canvasheight !== this.canvas.height) {
+			this.renderer.setSize(this.canvasWidth, this.canvasHeight, false);
+			this.camera.aspect = this.canvasWidth / this.canvasHeight;
+			this.camera.updateProjectionMatrix();
+			this.renderer.render(this.scene, this.camera)
+		}
 	}
 }
