ad38679ae27068b261bbf420b6d4b4a029743bce
Inklingboi <inklingboiii1@gmail.com>
2021-07-05 19:47:31 +0200

    added side bar

--- src/App/components/NavBar/NavBar.jsx
+++ src/App/components/NavBar/NavBar.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 export default function NavBar({ page }) {
-	return(
-		<nav className="nav">
-			<a href="#" role="logo" className="nav__logo">Arcade Archive</a>
-			<ul className="nav__list">
-				<li>
-					<Link to="./" className={page === 0 ? 'nav__link--active' : 'nav__link'}>Home</Link>
-				</li>
-				<li>
-					<Link to="/Games" className={page === 1 ? 'nav__link--active' : 'nav__link'}>Games</Link>
-				</li>
-				<li>
-					<Link to="/Planned-Games" className={page === 1 ? 'nav__link--active' : 'nav__link'}>Planned Games</Link>
-				</li>
-			</ul>
+	const [navToggle, setNavToggle] = useState(false);
+ 	return(
+		<nav className='nav'>
+			<button type="button" onClick={() => setNavToggle(!navToggle)} className='nav__toggler'>
+			{
+				navToggle ? 'UnToggle'
+				: 'Toggle'
+			}
+			</button>
+			<article className={`nav__content ${navToggle ? 'nav__content--active' : 'nav__content--inactive'}`}>
+				<a href="#" role="logo" className="nav__logo">Arcade Archive</a>
+				<ul className="nav__list">
+					<li>
+						<Link to="./" className={page === 0 ? 'nav__link--active' : 'nav__link'}>Home</Link>
+					</li>
+					<li>
+						<Link to="/Games" className={page === 1 ? 'nav__link--active' : 'nav__link'}>Games</Link>
+					</li>
+					<li>
+						<Link to="/Planned-Games" className={page === 1 ? 'nav__link--active' : 'nav__link'}>Planned Games</Link>
+					</li>
+				</ul>
+			</article>
 		</nav>
 	);
 }
