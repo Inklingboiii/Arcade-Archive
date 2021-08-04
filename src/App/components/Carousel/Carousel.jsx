780b478dcb64991a684279f10f806eb7c2a05f87
Inklingboi <inklingboiii1@gmail.com>
2021-08-04 15:11:38 +0200

    added carousel

--- src/App/components/Carousel/Carousel.jsx
+++ src/App/components/Carousel/Carousel.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import carouselStyles from './carousel.module.css';
 
 export default function Carousel({ data }) {
+	const [cardId, cardIdSetter] = useState(0);
+
+	function decrementCardId() {
+		cardIdSetter((cardId) => cardId - 1);
+	}
+
+	function incrementCardId() {
+		cardIdSetter((cardId) => cardId + 1);
+	}
 	return(
 
-		<main>
-			<button className="btn">last</button>
-				<ul>
+		<main className={carouselStyles.main}>
+			<button className="btn" onClick={decrementCardId}>last</button>
+				<ul className={carouselStyles.cardContainer}>
 					{
-						data.games.map((game, index) => {
+						data.games.map((game) => {
 							return( 
-							<li key={index}>
+							<li key={game.id} className={`${carouselStyles.card} ${game.id === cardId ? carouselStyles.cardActive : ''}`}>
 								<img src={game.img} alt={game.name} />
 								<h2>{game.name}</h2>
 								<p>{game.desc}</p>
@@ -19,7 +29,7 @@ export default function Carousel({ data }) {
 						})
 					}
 				</ul>
-			<button className="btn">next</button>
+			<button className="btn" onClick={incrementCardId}>next</button>
 		</main>
 	);
 }
