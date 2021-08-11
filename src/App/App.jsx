c50f73e69e13054c7770c99ee357652d863b7ebe
Inklingboi <inklingboiii1@gmail.com>
2021-08-11 11:20:03 +0200

    made wbeiste scroll up on page change

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
  Route,
- Switch
+ Switch,
+ useLocation
 } from 'react-router-dom';
 import Background from './components/Background/Background.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
@@ -10,6 +11,13 @@ import Games from './components/pages/Games/Games.jsx';
 import PlannedGames from './components/pages/PlannedGames/PlannedGames.jsx';
 import './App.css';
 export default function App() {
+	// Sets scrollbar to top when link changes: 
+	// https://stackoverflow.com/questions/36904185/react-router-scroll-to-top-on-every-transition
+	
+	const { pathname } = useLocation();
+  	useEffect(() => {
+    	window.scrollTo(0, 0);
+  	}, [pathname]);
 	return (
 		<>
 			<Background />
